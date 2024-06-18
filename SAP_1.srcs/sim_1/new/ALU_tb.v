281a24f9f0eb1aaad0dbe4ac741629fc6532fdd5
Thomas Whitford <twhitford2224@gmail.com>
2024-06-18 21:21:09 +0100

    ALU com

---
+++ SAP_1.srcs/sim_1/new/ALU_tb.v
@@ -0,0 +1,59 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 18.06.2024 20:37:05
+// Design Name: 
+// Module Name: ALU_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ALU_tb(
+
+    );
+
+    reg [7:0] A; // 2-bit input A
+    reg [7:0] B; // 2-bit input B
+    reg sub = 1'b0;
+    wire [7:0] Sum; // 3-bit output Sum
+    
+    // Instantiate the two_bit_adder module
+    ALU uut (
+        .i_a(A),
+        .i_b(B),
+        .i_sub(sub),
+        .o_out(Sum)
+    );
+    
+    initial begin
+        // Apply test vectors
+        A = 8'b00000000; B = 8'b00000000; #10; // Sum should be 000000000
+        A = 8'b00000001; B = 8'b00000001; #10; // Sum should be 000000010
+        A = 8'b11111111; B = 8'b00000001; #10; // Sum should be 100000000 (overflow)
+        A = 8'b10101010; B = 8'b01010101; #10; // Sum should be 111111111
+        A = 8'b01111111; B = 8'b01111111; #10; // Sum should be 111111110
+        sub = 1'b1;
+        A = 8'b00000000; B = 8'b00000000; #10; // Sum should be 000000000
+        A = 8'b00000000; B = 8'b00000001; #10; // Sum should be 000000010
+        A = 8'b11111111; B = 8'b00000001; #10; // Sum should be 100000000 (overflow)
+        A = 8'b10101010; B = 8'b01010101; #10; // Sum should be 111111111
+        A = 8'b01111111; B = 8'b01111111; #10; // Sum should be 111111110
+        // Finish simulation
+        $stop;
+    end
+    
+
+    
+    endmodule
--- SAP_1.srcs/sources_1/new/ALU.v
+++ SAP_1.srcs/sources_1/new/ALU.v
@@ -23,30 +23,26 @@
 module ALU(
 
     //inputs
-    input i_clk,
-    input i_rst,
     input i_sub, //1'b1 = Subtract, 1'b0 = Addition
     input [7:0]i_a,
     input [7:0]i_b,
-
+ 
     //outputs
-    output [7:0]o_ALU_out
+    output [7:0]o_out
 
     );
 
 
     //template
     // ALU(
-    //     .i_clk(),
-    //     .i_rst(),
     //     .i_sub(),
     //     .i_a(),
-    //     .i_b()
-    //     .o_ALU_out()
+    //     .i_b(),
+    //     .o_out()
     // )
 
 
-    assign o_ALU_out = (i_sub) ? i_a - i_b : i_a + i_b;
+    assign o_out = (i_sub) ? i_a-i_b : i_a+i_b;
 
 
 endmodule
